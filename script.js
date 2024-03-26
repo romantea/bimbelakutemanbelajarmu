493f7ac49b97ad4ed8505e2f9ade7bffbebbdd7e
roman <157903347+romantea@users.noreply.github.com>
2024-03-26 17:10:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// toggle
+const navbarNav = document.querySelector ('.navbar-nav');
+// ketika menu di klik
+document.querySelector ('#hamburger').onclick = () => {
+    navbarNav.classList.toggle ('active');
+}; 
+
+// klik di luar sidebar
+const hamburger = document.querySelector('#hamburger');
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+})
